47f207a90af17a03f75cc785a627a966825b10d9
Aakilahamed006 <ahamedaakil006@gmail.com>
2025-12-30 18:58:15 +0530

    Initial commit with full Spring Boot project

---
+++ src/main/java/com/academiccalender/AcademicCalanderApplication.java
@@ -0,0 +1,13 @@
+package com.academiccalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicCalanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicCalanderApplication.class, args);
+    }
+
+}

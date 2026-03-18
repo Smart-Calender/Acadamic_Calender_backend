0a8c866225d86f0a023a61562d809250b202130e
Aakilahamed006 <ahamedaakil006@gmail.com>
2026-03-18 16:19:09 +0530

    ready for hosting

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-java -jar backend/academicCalander-0.0.1-SNAPSHOT.jar
+
 
 #!/bin/sh
 apt-get update
 apt-get install -y openjdk-17-jdk
 
-java -jar backend/academicCalander-0.0.1-SNAPSHOT.jar
+#!/bin/bash
+java -jar backend/target/academicCalander-0.0.1-SNAPSHOT.jar

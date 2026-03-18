d31e94b704415b221848199eed0e00c9aacc10ea
Aakilahamed006 <ahamedaakil006@gmail.com>
2026-03-18 16:23:25 +0530

    ready for hosting

--- start.sh
+++ start.sh
@@ -3,11 +3,9 @@
 # Go to backend
 cd backend
 
-# Make sure Maven wrapper is executable
-chmod +x mvnw
 
-# Build the project
-./mvnw clean package -DskipTests
+
+
 
 # Run the JAR
 java -jar target/academicCalander-0.0.1-SNAPSHOT.jar

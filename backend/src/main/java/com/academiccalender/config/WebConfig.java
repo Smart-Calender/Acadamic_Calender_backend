48b552c4b6ade72f95e76a10cbbbc43b69e747de
Aakilahamed006 <ahamedaakil006@gmail.com>
2026-03-19 23:44:50 +0530

    Attendenc Trackinig Final Phase 1

--- backend/src/main/java/com/academiccalender/config/WebConfig.java
+++ backend/src/main/java/com/academiccalender/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")      // allow all endpoints
-                        .allowedOrigins("http://localhost:58451") // your frontend
+                        .allowedOrigins("https://acedemiccalender.netlify.app") // your frontend
                         .allowedMethods("GET","POST","PUT","DELETE")
                         .allowCredentials(true);
             }
